fd8f7f6b6230f941de937f37a0b38d450766c060
aub2136875 <aub2136875@maricopa.edu>
2026-01-27 18:49:37 -0700

    Adding Hello Sandbox

--- src/main/java/org/example/sandbox/hello/Hello.java
+++ src/main/java/org/example/sandbox/hello/Hello.java
@@ -2,6 +2,6 @@ package org.example.sandbox.hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, Sandbox!");
     }
 }
